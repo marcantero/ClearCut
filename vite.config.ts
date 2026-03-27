c8c8392219c97459cb6336599463a0fe8143cd28
Marc Cantero <148981890+NIU1710710@users.noreply.github.com>
2026-03-27 23:03:37 +0100

    vv

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/ClearCut/',
   plugins: [react()],
   optimizeDeps: {
     // Transformers.js incluye código avanzado que no se lleva bien
